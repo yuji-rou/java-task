bb7208b0ea8e337913e852d24116afb18a9b9a2c
yujirou <yujirou@example.com>
2023-02-02 15:48:35 +0900

    課題２

--- TryCatchFinallySample.java
+++ TryCatchFinallySample.java
@@ -1,23 +1,12 @@
-import java.io.*;
-
 public class TryCatchFinallySample {
     public static void main (String[] args) {
-        FileWriter fw = null;
         try {
-            fw = new FileWriter("date.txt");
-            fw.write("try");
             System.out.println("try");
-        } catch (IOException e) {
+            Integer.parseInt(args[0]);
+        } catch (Exception e) {
             System.out.println("catch");
         } finally {
-            if (fw != null) {
-                try {
-                    System.out.println("finally");             
-                    fw.close();
-                } catch (IOException e) {
-                  ;  
-                }    
-            }
+            System.out.println("finally");
         }
     }    
 }
