cc8c2eef967593069e5b661525f7f651752f6eda
yujirou <yujirou@example.com>
2023-02-12 21:14:33 +0900

    課題  1. 人を表す Person クラスを作成しましょう

--- Person.java
+++ Person.java
@@ -1,15 +1,12 @@
 public class Person {
   public enum Gender {MAN("man"),WOMAN("woman");
 
-    //文字列を保持するフィールド
     private final String text;
 
-    //コンストラクタ
     private Gender(final String text) {
         this.text = text;
     }
 
-    //文字列取得用メソッド
     public String getValue() {
         return this.text;
     }
